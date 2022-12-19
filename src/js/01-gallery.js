08d8cc3d21a86d3117a1a0375813d45664ce4afe
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2022-12-19 22:15:39 +0200

    Installed all the modules

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const gallery = createGalleryMarkup(galleryItems);
+
+galleryContainer.addEventListener('click', onGalleryClick);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+<div class="gallery">
+  <a href="${original}">
+    <img src="${preview}" alt="${description}" title="" />
+  </a>
+</div>
+      `;
+    })
+    .join('');
+}
+
+
+
+
+galleryContainer.insertAdjacentHTML('beforeend', gallery);
+function onGalleryClick(event) {
+  event.preventDefault();
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player('iframe');
+
+
+player.on('play', function () {
+localStorage.setItem('videoplayer-current-time', seconds);
+});
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+  });
 
