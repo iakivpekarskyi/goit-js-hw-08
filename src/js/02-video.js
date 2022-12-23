3f735259059ecb0995bd854675f56bc27b275689
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2022-12-23 11:26:20 +0200

    added eventlisteners and functions but got stuck

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,38 @@
+
+
+
+
+// Імпортуєм модуль vimeo player 
 import Player from '@vimeo/player';
 
+
+// Дадаємо до поеера метод throttle
+import throttle from 'lodash.throttle';
+
+// Отримуємо наш плеер з HTML 
 const iframe = document.querySelector('iframe');
-const player = new Player('iframe');
+
+// Встановлюєм(ініціалізуємо) наш плеер в плеер VIMEO, щоб ним можна було керувати.
+const player = new Player(iframe);
+
+// В плеері додаємо слухач подій у вигляді методу "on" разом, потім як параметр передаємо подію "timeupdate" - яка починає працювати щойно час у відео змінюється. і другим параментром додаємо функцию, яка додає ключ в localStorage.
+player.on('timeupdate', throttle(saveTime, 1000));
+
+// Функція "saveTime" додає в localStorage ключ "videoplayer-current-time" і його значення в секундах.
+function saveTime(event) {
+   localStorage.setItem('videoplayer-current-time', JSON.stringify (event.seconds));
+   console.log(set);
+
+};
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time',) || 0);
+ 
+
+// Дадаємо до поеера метод "setCurrentTime", для того щоб вказати звідкіля треба починати відео. Для цього замість конкретного часу ми передаємо в параметр не секунди, а дані які ми записали в localStorage 
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+console.log(currentTime);
 
 
-player.on('play', function () {
-localStorage.setItem('videoplayer-current-time', seconds);
-});
 
-player
-  .setCurrentTime(30.456)
-  .then(function (seconds) {
-  });
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+const KEY = 'feedback-form-state';
+
+
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+
+function isThereAnInput(evt) {
+   const savedInput = localStorage.getItem(KEY)
+   if (savedInput) {
+}
+};
+ 
+function onFormData(evt) {
+   const formData = {email: email.value,message: message.value};
+   const textIn = localStorage.setItem(KEY, JSON.stringify(formData));
+    console.log(textIn)
+
+
+};
+
+
+function onFormSubmit(evt) { 
+   evt.preventDefault();
+   evt.target.reset();
+   localStorage.removeItem(KEY);
+};
+
+
 
