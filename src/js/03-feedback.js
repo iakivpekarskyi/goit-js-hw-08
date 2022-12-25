3e343aa50973655387c5097be2450dbd0c967712
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2022-12-25 20:05:21 +0200

    done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,24 @@
+
+
 import throttle from 'lodash.throttle';
+
+
 
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('[name="email"]');
 const message = document.querySelector('[name="message"]');
-
 const KEY = 'feedback-form-state';
+
 
 
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onFormSubmit);
 
-
-function isThereAnInput(evt) {
-   const savedInput = localStorage.getItem(KEY)
-   if (savedInput) {
-}
-};
  
 function onFormData(evt) {
    const formData = {email: email.value,message: message.value};
    const textIn = localStorage.setItem(KEY, JSON.stringify(formData));
-   console.log(textIn);
-
-
 };
-
 
 function onFormSubmit(evt) { 
    evt.preventDefault();
@@ -32,5 +26,13 @@ function onFormSubmit(evt) {
    localStorage.removeItem(KEY);
 };
 
+updateInput();
 
+function updateInput(evt) {
+   const savedInput = JSON.parse(localStorage.getItem(KEY))
+   if (savedInput) {
+      email.value = savedInput.email;
+      message.value = savedInput.message;
+}
+}; 
 
