b76a419a1fa9b07104ab0814557bce0d02610a51
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2022-12-25 20:08:26 +0200

    formatted with prettier

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,20 +1,13 @@
-
-
 import throttle from 'lodash.throttle';
-
-
 
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('[name="email"]');
 const message = document.querySelector('[name="message"]');
 const KEY = 'feedback-form-state';
 
-
-
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onFormSubmit);
 
- 
 function onFormData(evt) {
    const formData = {email: email.value,message: message.value};
    const textIn = localStorage.setItem(KEY, JSON.stringify(formData));
